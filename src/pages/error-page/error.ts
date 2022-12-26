77fa8c76b80d52f4cd4a657ad2f5c224428ad085
Varvara <varyagagarina@gmail.com>
2022-12-26 21:45:56 +0300

    feat: add 404 layout

--- src/pages/error-page/error.ts
+++ src/pages/error-page/error.ts
@@ -3,7 +3,16 @@ import Page from '../../components/page';
 class ErrorPage extends Page {
   static CodeBlock = {
     MainTitle: `
-      <div class="error__title">Ошибка 404. Страница не найдена</div>
+      <div class="error">
+        <div class="container">
+          <div class="error__block">
+            <h1 class="error__title">Oops!</h1>
+            <h2 class="error__subtitle">YOU'RE LOST...</h2>
+            <h3 class="error__code">Error code: 404</h3>
+            <button class="button error__button">BACK TO HOMEPAGE</button>
+          </div>
+        </div>
+      </div>
     `,
   };
 
