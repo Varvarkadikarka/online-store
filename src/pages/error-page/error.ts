f18718c7f11cdc26f0676a45c1fbcd76a599219c
Varvara <varyagagarina@gmail.com>
2022-12-27 20:28:46 +0300

    fix: change layout for error page

--- src/pages/error-page/error.ts
+++ src/pages/error-page/error.ts
@@ -3,15 +3,11 @@ import Page from '../../components/page';
 class ErrorPage extends Page {
   static CodeBlock = {
     MainTitle: `
-      <div class="error">
-        <div class="container">
-          <div class="error__block">
-            <h1 class="error__title">Oops!</h1>
-            <h2 class="error__subtitle">YOU'RE LOST...</h2>
-            <h3 class="error__code">Error code: 404</h3>
-            <button class="button error__button">BACK TO HOMEPAGE</button>
-          </div>
-        </div>
+      <div class="error__block">
+        <h1 class="error__title">Oops!</h1>
+        <h2 class="error__subtitle">YOU'RE LOST...</h2>
+        <h3 class="error__code">Error code: 404</h3>
+        <button class="button error__button">BACK TO HOMEPAGE</button>
       </div>
     `,
   };
